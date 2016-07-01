dd2ca0c6b8414801aa686f0698ba2d0b7253b554
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-07-01 17:57:18 +0200

    Update DefaultContent links.

--- src/components/DefaultContent.js
+++ src/components/DefaultContent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-import { Button, Jumbotron } from "react-bootstrap";
+import { Link } from "react-router";
+import { Jumbotron } from "react-bootstrap";
 
 
 export default class DefaultContent extends Component {
@@ -11,10 +12,7 @@ export default class DefaultContent extends Component {
         <h1>Welcome to Kept</h1>
         <p>Your list is currently empty.</p>
         <p>You can create
-          a <a className="new-text" href="#" onClick={this.handleClickNewText}>Text</a>,
-          a <a className="new-todo" href="#" onClick={this.handleClickNewTodo}>Todo</a> or
-          <Button bsStyle="success" bsSize="large"
-                  onClick={this.handleClickLoadSamples}>Load samples</Button>.
+          a <Link className="new-text" to={`/create`}>Text</Link>.
         </p>
       </Jumbotron>
     );
